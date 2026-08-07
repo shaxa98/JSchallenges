38cc65107997f9d5c553288a3fcdab92cbaf4f01
shaxa98 <shakhzodakhrorov@gmail.com>
2026-08-07 10:29:50 +0500

    var

---
+++ app.js
@@ -0,0 +1,14 @@
+var Shaxzod = "Salom Shaxzod";
+// vareyble. bu variable o'zgaruvchidir. uni qayta o'zgartirish mumkin
+let Samir = "Salom Samir";
+// let o'zgaruvchi. uni qayta o'zgartirish mumkin
+const Javohir = "Salom Javohir";
+// const o'zgaruvchi. uni qayta o'zgartirish mumkin emas.
+//  const o'zgaruvchisi faqat bir marta qiymat oladi va o'zgarmaydi
+
+Shaxzod = "Salom Shaxzod 2";
+Samir = "Salom Samir 2";
+Javohir = "Salom Javohir 2";
+console.log(Shaxzod);
+console.log(Samir);
+console.log(Javohir);
