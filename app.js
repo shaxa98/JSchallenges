b39c44fe9804a6b82b4817514429d4b8d623c210
shaxa98 <shakhzodakhrorov@gmail.com>
2026-08-07 11:00:45 +0500

    1234

--- app.js
+++ app.js
@@ -8,7 +8,25 @@ const Javohir = "Salom Javohir";
 
 Shaxzod = "Salom Shaxzod 2";
 Samir = "Salom Samir 2";
-Javohir = "Salom Javohir 2";
+//Javohir = "Salom Javohir 2";
 console.log(Shaxzod);
 console.log(Samir);
 console.log(Javohir);
+
+// Boolian. true yoki false qiymat oladi
+
+let bir = 11 == 10; //tengmi
+let ikki = 12 > 15; // katta mi
+let uch = 10 < 15; // kichkinami
+let turt = 9 >= 10; // katta yoki tengmi
+let besh = 11 <= 10; // kichkina yoki tengmi
+let olti = "Salom" === "Salom";
+// tengmi harfni katta kichikligiga qarab
+let yetti = 10 != 10; // teng emasmi
+console.log(bir); //true
+console.log(ikki); //false
+console.log(uch); //true
+console.log(turt); //false
+console.log(besh); //false
+console.log(olti); //false
+console.log(yetti); //false
