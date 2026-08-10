0ddbe28a4174865ffab281465ea265253287168f
shaxa98 <shakhzodakhrorov@gmail.com>
2026-08-10 08:10:48 +0500

    daraja

--- app.js
+++ app.js
@@ -1,46 +1,72 @@
-var Shaxzod = "Salom Shaxzod";
-// vareyble. bu variable o'zgaruvchidir. uni qayta o'zgartirish mumkin
-let Samir = "Salom Samir";
-// let o'zgaruvchi. uni qayta o'zgartirish mumkin
-const Javohir = "Salom Javohir";
-// const o'zgaruvchi. uni qayta o'zgartirish mumkin emas.
-//  const o'zgaruvchisi faqat bir marta qiymat oladi va o'zgarmaydi
+// var Shaxzod = "Salom Shaxzod";
+// // vareyble. bu variable o'zgaruvchidir. uni qayta o'zgartirish mumkin
+// let Samir = "Salom Samir";
+// // let o'zgaruvchi. uni qayta o'zgartirish mumkin
+// const Javohir = "Salom Javohir";
+// // const o'zgaruvchi. uni qayta o'zgartirish mumkin emas.
+// //  const o'zgaruvchisi faqat bir marta qiymat oladi va o'zgarmaydi
 
-Shaxzod = "Salom Shaxzod 2";
-Samir = "Salom Samir 2";
-//Javohir = "Salom Javohir 2";
-console.log(Shaxzod);
-console.log(Samir);
-console.log(Javohir);
+// Shaxzod = "Salom Shaxzod 2";
+// Samir = "Salom Samir 2";
+// //Javohir = "Salom Javohir 2";
+// console.log(Shaxzod);
+// console.log(Samir);
+// console.log(Javohir);
 
-// Boolian. true yoki false qiymat oladi
+// // Boolian. true yoki false qiymat oladi
 
-let bir = 11 == 10; //tengmi
-let ikki = 12 > 15; // katta mi
-let uch = 10 < 15; // kichkinami
-let turt = 9 >= 10; // katta yoki tengmi
-let besh = 11 <= 10; // kichkina yoki tengmi
-let olti = "Salom" === "Salom";
-// tengmi harfni katta kichikligiga qarab
-let yetti = 10 != 10; // teng emasmi
-console.log(bir); //true
-console.log(ikki); //false
-console.log(uch); //true
-console.log(turt); //false
-console.log(besh); //false
-console.log(olti); //false
-console.log(yetti); //false
+// let bir = 11 == 10; //tengmi
+// let ikki = 12 > 15; // katta mi
+// let uch = 10 < 15; // kichkinami
+// let turt = 9 >= 10; // katta yoki tengmi
+// let besh = 11 <= 10; // kichkina yoki tengmi
+// let olti = "Salom" === "Salom";
+// // tengmi harfni katta kichikligiga qarab
+// let yetti = 10 != 10; // teng emasmi
+// console.log(bir); //true
+// console.log(ikki); //false
+// console.log(uch); //true
+// console.log(turt); //false
+// console.log(besh); //false
+// console.log(olti); //false
+// console.log(yetti); //false
 
-//  agar bolsa shartni tekshirish uchun ishlatiladi.
-//  agar shart to'g'ri bo'lsa, if ichidagi kod ishlaydi.
-//   agar shart noto'g'ri bo'lsa, else ichidagi kod ishlaydi.
-// else if shartni tekshirish uchun ishlatiladi.
-// agar shart to'g'ri bo'lsa, else if ichidagi kod ishlaydi.
-//  else if bi if dan keyin bir necha marta ishlatilishi mumkin.
-if (10 == 11) {
-  console.log("Shart to'g'ri");
-} else if (10 == 12) {
-  console.log("Shart to'g'ri 2");
-} else {
-  console.log("Shart noto'g'ri");
+// //  agar bolsa shartni tekshirish uchun ishlatiladi.
+// //  agar shart to'g'ri bo'lsa, if ichidagi kod ishlaydi.
+// //   agar shart noto'g'ri bo'lsa, else ichidagi kod ishlaydi.
+// // else if shartni tekshirish uchun ishlatiladi.
+// // agar shart to'g'ri bo'lsa, else if ichidagi kod ishlaydi.
+// //  else if bi if dan keyin bir necha marta ishlatilishi mumkin.
+// if (10 == 11) {
+//   console.log("Shart to'g'ri");
+// } else if (10 == 12) {
+//   console.log("Shart to'g'ri 2");
+// } else {
+//   console.log("Shart noto'g'ri");
+// }
+var son = 2;
+
+function SonidarajasiniAniqlash(son) {
+var 1Daraja = son * son;
+var 2Daraja = son * son * son;
+var 3Daraja = son * son * son * son;
+var 4Daraja = son * son * son * son * son;
+var 5Daraja = son * son * son * son * son * son;
+var 6Daraja = son * son * son * son * son * son * son;
+var 7Daraja = son * son * son * son * son * son * son * son;
+var 8Daraja = son * son * son * son * son * son * son * son * son;
+var 9Daraja = son * son * son * son * son * son * son * son * son * son;
+var 10Daraja = son * son * son * son * son * son * son * son * son * son * son;
+
+console.log("1-daraja: " + 1Daraja);
+console.log("2-daraja: " + 2Daraja);
+console.log("3-daraja: " + 3Daraja);
+console.log("4-daraja: " + 4Daraja);
+console.log("5-daraja: " + 5Daraja);
+console.log("6-daraja: " + 6Daraja);
+console.log("7-daraja: " + 7Daraja);
+console.log("8-daraja: " + 8Daraja);
+console.log("9-daraja: " + 9Daraja);
+console.log("10-daraja: " + 10Daraja);
+
 }
