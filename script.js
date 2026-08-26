bea336f68d14905b746c3477b2889c4aa23d4c58
shaxa98 <shakhzodakhrorov@gmail.com>
2026-08-26 09:45:46 +0500

    Comit

--- script.js
+++ script.js
@@ -1,295 +1,295 @@
-var Shaxzod = "Salom Shaxzod";
-// vareyble. bu variable o'zgaruvchidir. uni qayta o'zgartirish mumkin
-let Samir = "Salom Samir";
-// let o'zgaruvchi. uni qayta o'zgartirish mumkin
-const Javohir = "Salom Javohir";
-// const o'zgaruvchi. uni qayta o'zgartirish mumkin emas.
-//  const o'zgaruvchisi faqat bir marta qiymat oladi va o'zgarmaydi
+// var Shaxzod = "Salom Shaxzod";
+// // vareyble. bu variable o'zgaruvchidir. uni qayta o'zgartirish mumkin
+// let Samir = "Salom Samir";
+// // let o'zgaruvchi. uni qayta o'zgartirish mumkin
+// const Javohir = "Salom Javohir";
+// // const o'zgaruvchi. uni qayta o'zgartirish mumkin emas.
+// //  const o'zgaruvchisi faqat bir marta qiymat oladi va o'zgarmaydi
 
-Shaxzod = "Salom Shaxzod 2";
-Samir = "Salom Samir 2";
-//Javohir = "Salom Javohir 2";
-console.log(Shaxzod);
-console.log(Samir);
-console.log(Javohir);
+// Shaxzod = "Salom Shaxzod 2";
+// Samir = "Salom Samir 2";
+// //Javohir = "Salom Javohir 2";
+// console.log(Shaxzod);
+// console.log(Samir);
+// console.log(Javohir);
 
-// Boolian. true yoki false qiymat oladi
+// // Boolian. true yoki false qiymat oladi
 
-let bir = 11 == 10; //tengmi
-let ikki = 12 > 15; // katta mi
-let uch = 10 < 15; // kichkinami
-let turt = 9 >= 10; // katta yoki tengmi
-let besh = 11 <= 10; // kichkina yoki tengmi
-let olti = "Salom" === "Salom";
-// tengmi harfni katta kichikligiga qarab
-let yetti = 10 != 10; // teng emasmi
-console.log(bir); //true
-console.log(ikki); //false
-console.log(uch); //true
-console.log(turt); //false
-console.log(besh); //false
-console.log(olti); //false
-console.log(yetti); //false
+// let bir = 11 == 10; //tengmi
+// let ikki = 12 > 15; // katta mi
+// let uch = 10 < 15; // kichkinami
+// let turt = 9 >= 10; // katta yoki tengmi
+// let besh = 11 <= 10; // kichkina yoki tengmi
+// let olti = "Salom" === "Salom";
+// // tengmi harfni katta kichikligiga qarab
+// let yetti = 10 != 10; // teng emasmi
+// console.log(bir); //true
+// console.log(ikki); //false
+// console.log(uch); //true
+// console.log(turt); //false
+// console.log(besh); //false
+// console.log(olti); //false
+// console.log(yetti); //false
 
- agar bolsa shartni tekshirish uchun ishlatiladi.
- agar shart to'g'ri bo'lsa, if ichidagi kod ishlaydi.
-  agar shart noto'g'ri bo'lsa, else ichidagi kod ishlaydi.
-else if shartni tekshirish uchun ishlatiladi.
-agar shart to'g'ri bo'lsa, else if ichidagi kod ishlaydi.
- else if bi if dan keyin bir necha marta ishlatilishi mumkin.
-if (10 == 11) {
-  console.log("Shart to'g'ri");
-} else if (10 == 12) {
-  console.log("Shart to'g'ri 2");
-} else {
-  console.log("Shart noto'g'ri");
-}
+//  agar bolsa shartni tekshirish uchun ishlatiladi.
+//  agar shart to'g'ri bo'lsa, if ichidagi kod ishlaydi.
+//   agar shart noto'g'ri bo'lsa, else ichidagi kod ishlaydi.
+// else if shartni tekshirish uchun ishlatiladi.
+// agar shart to'g'ri bo'lsa, else if ichidagi kod ishlaydi.
+//  else if bi if dan keyin bir necha marta ishlatilishi mumkin.
+// if (10 == 11) {
+//   console.log("Shart to'g'ri");
+// } else if (10 == 12) {
+//   console.log("Shart to'g'ri 2");
+// } else {
+//   console.log("Shart noto'g'ri");
+// }
 
-array bu bir nechta qiymatlarni saqlash uchun ishlatiladi.
- array ichidagi qiymatlar indeks raqamlar bilan belgilanadi.
-array ichidagi qiymatlar 0 dan boshlanadi.
-array ichidagi qiymatlar o'zgartirilishi mumkin.
-array ichidagi qiymatlar qo'shilishi mumkin.
-array ichidagi qiymatlar o'chirilishi mumkin.
-array ichidagi qiymatlar tartiblanishi mumkin.
-array ichidagi qiymatlar filtrlash mumkin.
-array ichidagi qiymatlar map qilish mumkin.
-array ichidagi qiymatlar reduce qilish mumkin.
-array ichidagi qiymatlar find qilish mumkin.
-array ichidagi qiymatlar some qilish mumkin.
-array ichidagi qiymatlar every qilish mumkin.
-array
-var arr = [1, 2, 3, 4, 5];
+// array bu bir nechta qiymatlarni saqlash uchun ishlatiladi.
+//  array ichidagi qiymatlar indeks raqamlar bilan belgilanadi.
+// array ichidagi qiymatlar 0 dan boshlanadi.
+// array ichidagi qiymatlar o'zgartirilishi mumkin.
+// array ichidagi qiymatlar qo'shilishi mumkin.
+// array ichidagi qiymatlar o'chirilishi mumkin.
+// array ichidagi qiymatlar tartiblanishi mumkin.
+// array ichidagi qiymatlar filtrlash mumkin.
+// array ichidagi qiymatlar map qilish mumkin.
+// array ichidagi qiymatlar reduce qilish mumkin.
+// array ichidagi qiymatlar find qilish mumkin.
+// array ichidagi qiymatlar some qilish mumkin.
+// array ichidagi qiymatlar every qilish mumkin.
+// array
+// var arr = [1, 2, 3, 4, 5];
 
-console.log(arr[4]);
+// console.log(arr[4]);
 
-var bollalar = "Salohiddin, Surob, Abdulboqi, Javohir";
-console.log(bollalar);
+// var bollalar = "Salohiddin, Surob, Abdulboqi, Javohir";
+// console.log(bollalar);
 
-var blalar = ["Salohiddin", "Surob", "Abdulboqi", "Javohir"];
-console.log(blalar[2]);
+// var blalar = ["Salohiddin", "Surob", "Abdulboqi", "Javohir"];
+// console.log(blalar[2]);
 
-var yosh = 20;
-if (yosh == 7) {
-  console.log("Birinchi Sinf");
-} else if (yosh == 8) {
-  console.log("Ikkinchi Sinf");
-} else if (yosh == 9) {
-  console.log("Uchinchi sinf");
-} else if (yosh == 10) {
-  console.log("To`rtinchi sinf");
-} else if (yosh == 11) {
-  console.log("Beshinchi Sinf");
-} else if (yosh == 12) {
-  console.log("Oltinchi Sinf");
-} else if (yosh == 13) {
-  console.log("Yettinchi Sinf");
-} else if (yosh == 14) {
-  console.log("Sakkizinchi Sinf");
-} else if (yosh == 15) {
-  console.log("To`qqizinchi Sinf");
-} else if (yosh == 16) {
-  console.log("O`ninchi Sinf");
-} else if (yosh == 17) {
-  console.log("O`nbirinchi Sinf");
-} else if (yosh < 7) {
-  console.log("Maktab Yoshiga yetmagan");
-} else {
-  console.log("Maktab yoshidan o`ttib ketgan");
-}
+// var yosh = 20;
+// if (yosh == 7) {
+//   console.log("Birinchi Sinf");
+// } else if (yosh == 8) {
+//   console.log("Ikkinchi Sinf");
+// } else if (yosh == 9) {
+//   console.log("Uchinchi sinf");
+// } else if (yosh == 10) {
+//   console.log("To`rtinchi sinf");
+// } else if (yosh == 11) {
+//   console.log("Beshinchi Sinf");
+// } else if (yosh == 12) {
+//   console.log("Oltinchi Sinf");
+// } else if (yosh == 13) {
+//   console.log("Yettinchi Sinf");
+// } else if (yosh == 14) {
+//   console.log("Sakkizinchi Sinf");
+// } else if (yosh == 15) {
+//   console.log("To`qqizinchi Sinf");
+// } else if (yosh == 16) {
+//   console.log("O`ninchi Sinf");
+// } else if (yosh == 17) {
+//   console.log("O`nbirinchi Sinf");
+// } else if (yosh < 7) {
+//   console.log("Maktab Yoshiga yetmagan");
+// } else {
+//   console.log("Maktab yoshidan o`ttib ketgan");
+// }
 
-let son = 7;
+// let son = 7;
 
-if (son % 2 === 0) {
-  console.log("Juft son");
-} else if (son % 2 === 1) {
-  console.log("Toq son");
-}
+// if (son % 2 === 0) {
+//   console.log("Juft son");
+// } else if (son % 2 === 1) {
+//   console.log("Toq son");
+// }
 
-let login = "admin";
-let password = "12345";
+// let login = "admin";
+// let password = "12345";
 
-if (login === "admin" && password === "12345") {
-  console.log("Kirish muvaffaqiyatli");
-} else {
-  console.log("Login yoki parol xato");
-}
+// if (login === "admin" && password === "12345") {
+//   console.log("Kirish muvaffaqiyatli");
+// } else {
+//   console.log("Login yoki parol xato");
+// }
 
-var raqam = 1; // Number
-var harf = "A B C"; // String
-var boolian = 10 == 10; // taqoslash boolian True/False
-var array = [1, 2, 3, 4];
-var array2 = ["A", "B", "C", "Anis"];
+// var raqam = 1; // Number
+// var harf = "A B C"; // String
+// var boolian = 10 == 10; // taqoslash boolian True/False
+// var array = [1, 2, 3, 4];
+// var array2 = ["A", "B", "C", "Anis"];
 
-console.log(array2[3]);
+// console.log(array2[3]);
 
-if (10 == 11) {
-  console.log(10 + " Teng " + 10);
-} else if (20 > 30) {
-  console.log(20 + " katta " + 15);
-} else if (40 < 30) {
-  console.log(40 + " katta " + 60);
-} else {
-  console.log("False");
-}
+// if (10 == 11) {
+//   console.log(10 + " Teng " + 10);
+// } else if (20 > 30) {
+//   console.log(20 + " katta " + 15);
+// } else if (40 < 30) {
+//   console.log(40 + " katta " + 60);
+// } else {
+//   console.log("False");
+// }
 
-let names = ["Ali", "Vali", "Hasan", "Husan"];
+// let names = ["Ali", "Vali", "Hasan", "Husan"];
 
-for (let i = 0; i < names.length; i++) {
-    console.log(names[i]);
-}
+// for (let i = 0; i < names.length; i++) {
+//     console.log(names[i]);
+// }
 
-for (var i = 1; i <= 100; i++) {
-  console.log(i);
-}
+// for (var i = 1; i <= 100; i++) {
+//   console.log(i);
+// }
 
-var Raqam = 15;
+// var Raqam = 15;
 
-for (var i = 1; i <= 10; i++) {
-  console.log(Raqam + " X " + i + " = " + Raqam * i);
-}
+// for (var i = 1; i <= 10; i++) {
+//   console.log(Raqam + " X " + i + " = " + Raqam * i);
+// }
 
-function Sekund(minut) {
-  console.log(minut + " bu " + minut * 60 + " sekundga teng!  ");
-}
-Sekund(5);
+// function Sekund(minut) {
+//   console.log(minut + " bu " + minut * 60 + " sekundga teng!  ");
+// }
+// Sekund(5);
 
-function Minut(soat) {
-  console.log(soat + " bu " + soat * 60 + " minutgaga teng!  ");
-}
-Minut(5);
+// function Minut(soat) {
+//   console.log(soat + " bu " + soat * 60 + " minutgaga teng!  ");
+// }
+// Minut(5);
 
-function Sutka(soat1) {
-  console.log(soat1 + " bu " + soat1 / 24 + " sutkaga teng!  ");
-}
-Sutka(36);
+// function Sutka(soat1) {
+//   console.log(soat1 + " bu " + soat1 / 24 + " sutkaga teng!  ");
+// }
+// Sutka(36);
 
-function GaplarniBirlashtirish(BirinchiGap, IkkinchiGap) {
-  return BirinchiGap + "  " + IkkinchiGap;
-}
+// function GaplarniBirlashtirish(BirinchiGap, IkkinchiGap) {
+//   return BirinchiGap + "  " + IkkinchiGap;
+// }
 
-console.log(GaplarniBirlashtirish("Salom ", "Bolajonlar! "));
+// console.log(GaplarniBirlashtirish("Salom ", "Bolajonlar! "));
 
-function Ikkibaravar(num) {
-  return num * 2;
-}
-console.log(Ikkibaravar(23));
+// function Ikkibaravar(num) {
+//   return num * 2;
+// }
+// console.log(Ikkibaravar(23));
 
-function IldizTopish(num) {
-  return Math.sqrt(num);
-}
+// function IldizTopish(num) {
+//   return Math.sqrt(num);
+// }
 
-console.log(IldizTopish(81));
-console.log(IldizTopish(360));
-console.log(IldizTopish(900));
-console.log(IldizTopish(760));
-console.log(IldizTopish(25));
+// console.log(IldizTopish(81));
+// console.log(IldizTopish(360));
+// console.log(IldizTopish(900));
+// console.log(IldizTopish(760));
+// console.log(IldizTopish(25));
 
-50 dan 100 50 ta ko'p
-let arr = [10, 20, 30, 40, 50, 100];
-function KattasoniTopish(arr) {
-  let katta = arr.sort((a, b) => b - a);
-  let son1 = katta[0];
-  let son2 = katta[1];
-  let son3 = son1 - son2;
-  console.log(son2 + " dan " + son1 + " " + son3 + " Ta Ko'p");
-}
+// 50 dan 100 50 ta ko'p
+// let arr = [10, 20, 30, 40, 50, 100];
+// function KattasoniTopish(arr) {
+//   let katta = arr.sort((a, b) => b - a);
+//   let son1 = katta[0];
+//   let son2 = katta[1];
+//   let son3 = son1 - son2;
+//   console.log(son2 + " dan " + son1 + " " + son3 + " Ta Ko'p");
+// }
 
-KattasoniTopish(arr);
+// KattasoniTopish(arr);
 
-[1, 2, 3, 4, 5, 6, 7, 8, 9]
-Bu yerda 9 ta son bor
+// [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// Bu yerda 9 ta son bor
 
-function arraygaAjrat(son) {
-  return String(son).split("").map(Number);
-}
+// function arraygaAjrat(son) {
+//   return String(son).split("").map(Number);
+// }
 
-let son = 123456789881234;
-let natija = arraygaAjrat(son);
-console.log(natija);
-console.log("Bu Yera ", natija.length, "ta son bor");
+// let son = 123456789881234;
+// let natija = arraygaAjrat(son);
+// console.log(natija);
+// console.log("Bu Yera ", natija.length, "ta son bor");
 
-let str = "Abdulboqi";
-let search = "y";
-// Abdulboyiyni ichida  y yo'q
+// let str = "Abdulboqi";
+// let search = "y";
+// // Abdulboyiyni ichida  y yo'q
 
-function harfTop(gap, harf) {
-  if (gap.includes(harf)) {
-    return `${gap} ichida ${harf} mavjud`;
-  } else {
-    return `${gap} ichida ${harf} mavjud emas`;
-  }
-}
+// function harfTop(gap, harf) {
+//   if (gap.includes(harf)) {
+//     return `${gap} ichida ${harf} mavjud`;
+//   } else {
+//     return `${gap} ichida ${harf} mavjud emas`;
+//   }
+// }
 
-console.log(harfTop("Abdulboqi", "u"));
-console.log(harfTop("Avaziddin", "u"));
-console.log(harfTop("So'rob", "q"));
-console.log(harfTop("Salohiddin", "d"));
+// console.log(harfTop("Abdulboqi", "u"));
+// console.log(harfTop("Avaziddin", "u"));
+// console.log(harfTop("So'rob", "q"));
+// console.log(harfTop("Salohiddin", "d"));
 
-let birinchiSon = 65;
-let ikkinchiSon = 90;
+// let birinchiSon = 65;
+// let ikkinchiSon = 90;
 
-//  30 dan 90gach bo'lgan foiz bu 30%
+// //  30 dan 90gach bo'lgan foiz bu 30%
 
-function foizHisobla(son1, son2) {
-  let foiz = ((son2 - son1) / son1) * 100;
-  return son1 + " dan " + son2 + " gacha bo'lgan foiz bu " + foiz + "%";
-}
+// function foizHisobla(son1, son2) {
+//   let foiz = ((son2 - son1) / son1) * 100;
+//   return son1 + " dan " + son2 + " gacha bo'lgan foiz bu " + foiz + "%";
+// }
 
-console.log(foizHisobla(65, 90));
-console.log(foizHisobla(80, 160));
+// console.log(foizHisobla(65, 90));
+// console.log(foizHisobla(80, 160));
 
-let arr = [1, 2, 4, 5, 3, 6, 7, 2, 7, 6, 8];
+// let arr = [1, 2, 4, 5, 3, 6, 7, 2, 7, 6, 8];
 
-function JuftToq(arr) {
-  let Juft = [];
-  let Toq = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 === 0) {
-      Juft.push(arr[i]);
-    } else {
-      Toq.push(arr[i]);
-    }
-  }
-  return [Juft, Toq];
-}
+// function JuftToq(arr) {
+//   let Juft = [];
+//   let Toq = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//       Juft.push(arr[i]);
+//     } else {
+//       Toq.push(arr[i]);
+//     }
+//   }
+//   return [Juft, Toq];
+// }
 
-console.log(JuftToq([1, 2, 4, 5, 3, 6, 7, 2, 7, 6, 8]));
-// [2, 4, 2, 6, 8]
-// [ 1, 5, 3, 7, 7]
+// console.log(JuftToq([1, 2, 4, 5, 3, 6, 7, 2, 7, 6, 8]));
+// // [2, 4, 2, 6, 8]
+// // [ 1, 5, 3, 7, 7]
 
-var Surob = 1234567890;
-// 1-2-3-4-5
+// var Surob = 1234567890;
+// // 1-2-3-4-5
 
-function sonUrtasigaQush(son) {
-  return String(son).split("").join("-");
-}
+// function sonUrtasigaQush(son) {
+//   return String(son).split("").join("-");
+// }
 
-console.log(sonUrtasigaQush(Surob));
+// console.log(sonUrtasigaQush(Surob));
 
-let arr = [50, 60, 80, 70, 90];
+// let arr = [50, 60, 80, 70, 90];
 
-function ortacha(arr) {
-  let summa = 0;
+// function ortacha(arr) {
+//   let summa = 0;
 
-  for (let i = 0; i < arr.length; i++) {
-    summa += arr[i];
-  }
+//   for (let i = 0; i < arr.length; i++) {
+//     summa += arr[i];
+//   }
 
-  return summa / arr.length;
-}
+//   return summa / arr.length;
+// }
 
-console.log(ortacha([30, 60, 50, 80, 90]));
-console.log(ortacha([45, 67, 90, 78, 100]));
+// console.log(ortacha([30, 60, 50, 80, 90]));
+// console.log(ortacha([45, 67, 90, 78, 100]));
 
-function kvadratlarYigindisi(n) {
-  let summa = 0;
+// function kvadratlarYigindisi(n) {
+//   let summa = 0;
 
-  for (let i = 1; i <= n; i++) {
-    summa += i * i;
-  }
+//   for (let i = 1; i <= n; i++) {
+//     summa += i * i;
+//   }
 
-  return summa;
-}
+//   return summa;
+// }
 
-console.log(kvadratlarYigindisi(7));
+// console.log(kvadratlarYigindisi(7));
